e0b2e72e6b51b2fa727ceaab2d79c5babbdf309e
Gia Khang <khanggia85@gmail.com>
2024-04-14 23:45:45 +0700

    added path to blocked screen

--- src/Components/BlockedScreen/BlockedScreen.tsx
+++ src/Components/BlockedScreen/BlockedScreen.tsx
@@ -7,14 +7,15 @@ type Props = {}
 
 const BlockedScreen = (props: Props) => {
   const { writeToClipboard } = useDeviceMethods();
+
   return (
     <div className="blockedScreen">
       <div className="title">CHÚ Ý</div>
       <div className="description">
         Eco không hỗ trợ trình duyệt của Messenger & Facebook, bạn vui lòng copy link và truy cập từ trình duyệt của thiết bị (Safari/Google Chrome/...)
         <div className="webUrl">
-          {GlobalConstants.webUrl}
-          <Button icon={<CopyFilled />} style={{ color: 'white' }} hideBorder onClick={() => writeToClipboard(GlobalConstants.webUrl)}></Button>
+          {GlobalConstants.webUrl}{window.location.pathname}
+          <Button icon={<CopyFilled />} style={{ color: 'white' }} hideBorder onClick={() => writeToClipboard(`${GlobalConstants.webUrl}${window.location.pathname}`)}></Button>
         </div>
       </div>
     </div>
