13c69fda47f217139da5412bfc15ec587df5fd91
Gia Khang <khanggia85@gmail.com>
2024-05-15 12:02:10 +0700

    upload new banner size

--- src/Services/Functions/DeviceMethods.tsx
+++
@@ -1,6 +0,0 @@
-import { message } from "antd";
-import { ISafeZone } from "../../Model/Others"
-import { GlobalConstants } from "../../Share/Constants"
-import { IUser } from "../../Model/Users";
-import copy from "copy-to-clipboard";
-
