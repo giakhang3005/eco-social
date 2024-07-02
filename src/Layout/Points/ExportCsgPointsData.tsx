2c8fd4b63f15fca6116a3116cb824e10b5df54c2
Gia Khang <khanggia85@gmail.com>
2024-07-03 00:21:24 +0700

    parse

--- src/Layout/Points/ExportCsgPointsData.tsx
+++ src/Layout/Points/ExportCsgPointsData.tsx
@@ -8,7 +8,7 @@ import { GlobalConstants } from '../../Share/Constants'
 type Props = {}
 
 const ExportCsgPointsData = (props: Props) => {
-    const { getCsgMember, getCurrentUser } = useUsers();
+    const { getCsgMember, getAllUsers } = useUsers();
     const { checkHavePerm } = usePermissions();
     const { updateLoading } = useLoading();
     const { exportExcel } = useExcel();
@@ -45,10 +45,52 @@ const ExportCsgPointsData = (props: Props) => {
         await exportExcel(exportedData, 'Data Eco Points CSG', excelName);
         updateLoading(false, "");
     }
+
+    const onExportAllData = async () => {
+        updateLoading(true, "Đang trích xuất...");
+
+        const allMembersList = await getAllUsers();
+
+        if (!allMembersList) {
+            updateLoading(false, "Đang trích xuất...");
+            return;
+        }
+
+        updateLoading(true, "Đang tải...");
+        const currTime = new Date();
+
+        const formatedList = allMembersList
+            .map((data) => (
+                {
+                    mssv: data.mssv,
+                    name: data.name,
+                    email: data.email,
+                    points: data.points,
+                    joinDate: data.joinDate,
+                    avatar: data.imgUrl
+                }
+            ))
+            .sort((a, b) => a.points - b.points);
+
+        const exportedTime = `${currTime.getHours()}:${currTime.getMinutes()}:${currTime.getSeconds()} _ ${currTime.getDate()}/${currTime.getMonth()}/${currTime.getFullYear()}`
+        const exportedData = [{ exportedTime: exportedTime }, ...formatedList]
+
+        const excelName = `CSG_Eco_Data_${currTime.getTime()}`;
+
+
+        await exportExcel(exportedData, 'Data Eco Social', excelName);
+        updateLoading(false, "");
+    }
+
     return (
         <>
             {checkHavePerm(GlobalConstants.permissionsKey.export) &&
-                <Button onClick={onExport} showIcon={false} showText style={{ margin: '0 0 0 20px' }}>Export Data CSG</Button>
+                (
+                    <>
+                        <Button onClick={onExport} showIcon={false} showText style={{ margin: '0 0 0 20px' }}>Export Data CSG</Button>
+                        <Button onClick={onExportAllData} showIcon={false} showText style={{ margin: '0 0 0 20px' }}>Export All Data</Button>
+                    </>
+                )
             }
         </>
     )
